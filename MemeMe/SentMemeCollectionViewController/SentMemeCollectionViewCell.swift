de97a7b6581fda0ce6d46315dbeef00bb6acecbb
Lester Batres <lester96@gmail.com>
2017-10-01 17:30:51 -0600

    -Added SentMemeCollectionViewController -Added SentMemeCollectionViewCell

---
+++ MemeMe/SentMemeCollectionViewController/SentMemeCollectionViewCell.swift
@@ -0,0 +1,13 @@
+//
+//  SentMemeCollectionViewCell.swift
+//  MemeMe
+//
+//  Created by Lester Batres on 10/1/17.
+//  Copyright © 2017 Lester Batres. All rights reserved.
+//
+
+import UIKit
+
+class SentMemeCollectionViewCell: UICollectionViewCell {
+    
+}
