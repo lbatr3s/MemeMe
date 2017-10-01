8f8c0799fcf8e77013a6eeb91f87619af1a565c4
Lester Batres <lester96@gmail.com>
2017-10-01 17:45:16 -0600

    -Added SentMemeTableViewController -Added SentMemeTableViewCell -Added TabNavigationController

---
+++ MemeMe/SentMemeTableViewController/SentMemeTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  SentMemeTableViewCell.swift
+//  MemeMe
+//
+//  Created by Lester Batres on 10/1/17.
+//  Copyright © 2017 Lester Batres. All rights reserved.
+//
+
+import UIKit
+
+class SentMemeTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
