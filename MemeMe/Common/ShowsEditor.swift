c944737c4eb2836b723a3275ea1dddccc214f3ab
Lester Batres <lester96@gmail.com>
2017-10-04 20:23:55 -0600

    -Added segue navigation -Added memes data source

--- MemeMe/Common/ShowsEditor.swift
+++ MemeMe/Common/ShowsEditor.swift
@@ -11,11 +11,16 @@ import UIKit
 protocol ShowsEditor {
     
     var selector: Selector { get }
+    var segueIdentifier: String { get }
     
     func addCreateMemeButton()
 }
 
 extension ShowsEditor where Self:  UIViewController {
+    
+    var segueIdentifier: String {
+        return "memeEditorSegueIdentifier"
+    }
     
     func addCreateMemeButton() {
         self.navigationItem.rightBarButtonItem = UIBarButtonItem(barButtonSystemItem: .add, target: self, action: selector)
